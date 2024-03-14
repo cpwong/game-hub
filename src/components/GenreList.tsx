00568d4f72a899860777177ee7ddee9ae7a721a7
CP Wong <wongchenpang@gmail.com>
2024-03-14 19:20:14 +0800

    Cleanup the Genres list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,14 @@
-import { Button, HStack, Image, List, ListItem, Spinner } from '@chakra-ui/react';
-import useGenres, { Genre } from '../hooks/useGenres';
-import getCroppedImageUrl from '../services/image-url';
+import {
+  Button,
+  HStack,
+  Heading,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+} from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
@@ -12,20 +20,33 @@ function GenreList({ onSelectGenre, selectedGenre }: Props) {
   if (error) return null;
   if (isLoading) return <Spinner />;
   return (
-    <List>
-      {data.map((gen) => (
-        <ListItem key={gen.id} paddingY='5px'>
-          <HStack>
-            <Image
-              boxSize='32px'
-              borderRadius={8}
-              src={getCroppedImageUrl(gen.image_background)}
-            />
-            <Button fontWeight={gen.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(gen)} fontSize='lg' variant='link'>{gen.name}</Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>Genres</Heading>
+      <List>
+        {data.map((gen) => (
+          <ListItem key={gen.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+                src={getCroppedImageUrl(gen.image_background)}
+              />
+              <Button
+                whiteSpace="normal"
+                textAlign="left"
+                fontWeight={gen.id === selectedGenre?.id ? "bold" : "normal"}
+                onClick={() => onSelectGenre(gen)}
+                fontSize="lg"
+                variant="link"
+              >
+                {gen.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 }
 
