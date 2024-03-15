50daf783d1a976142af0ce1ab06244fb736747ee
CP Wong <wongchenpang@gmail.com>
2024-03-15 12:11:43 +0800

    Change page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
-import { useState } from "react";
-import GameGrid from "./components/GameGrid";
-import GenreList from "./components/GenreList";
-import NavBar from "./components/NavBar";
-import PlatformSelector from "./components/PlatformSelector";
-import { Genre } from "./hooks/useGenres";
-import { Platform } from "./hooks/useGames";
-import SortSelector from "./components/SortSelector";
-import GameHeading from "./components/GameHeading";
+import { Box, Flex, Grid, GridItem, Show } from '@chakra-ui/react';
+import { useState } from 'react';
+import GameGrid from './components/GameGrid';
+import GameHeading from './components/GameHeading';
+import GenreList from './components/GenreList';
+import NavBar from './components/NavBar';
+import PlatformSelector from './components/PlatformSelector';
+import SortSelector from './components/SortSelector';
+import { Platform } from './hooks/useGames';
+import { Genre } from './hooks/useGenres';
 
 export interface GameQuery {
   genre: Genre | null;
@@ -27,26 +27,26 @@ function App() {
           lg: `"nav nav" "aside main"`,
         }}
         templateColumns={{
-          base: "1fr",
-          lg: "200px",
+          base: '1fr',
+          lg: '200px',
         }}
       >
-        <GridItem area="nav">
+        <GridItem area='nav'>
           <NavBar
             onSearch={(searchText) =>
               setGameQuery({ ...gameQuery, searchText })
             }
           />
         </GridItem>
-        <Show above="lg">
-          <GridItem area="aside" paddingX={5}>
+        <Show above='lg'>
+          <GridItem area='aside' paddingX={5}>
             <GenreList
               selectedGenre={gameQuery.genre}
               onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
             />
           </GridItem>
         </Show>
-        <GridItem area="main">
+        <GridItem area='main'>
           <Box paddingLeft={2}>
             <GameHeading gameQuery={gameQuery} />
             <Flex marginBottom={5}>
